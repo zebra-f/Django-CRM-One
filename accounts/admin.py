027b740eb466bb4c8c1348c5003ca6b312d2760e
zebra-f <rafal.zebrowski@gmail.com>
2022-05-17 12:30:54 +0200

    Products, Order

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,3 +5,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Customer)
+admin.site.register(models.Product)
+admin.site.register(models.Order)
--- accounts/models.py
+++ accounts/models.py
@@ -11,3 +11,31 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Product(models.Model):
+
+    CATEGORIES = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+
+    name = models.CharField(max_length=128, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=128, null=True, choices=CATEGORIES)
+    description = models.CharField(max_length=256, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    # customer 
+    # product 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=16, null=True, choices=STATUS)
