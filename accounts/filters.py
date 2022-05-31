9dbfd374fd95dd7c38b6a6c2809ccd87624e7534
zebra-f <rafal.zebrowski@gmail.com>
2022-05-31 14:10:38 +0200

    product__category

--- accounts/filters.py
+++ accounts/filters.py
@@ -4,7 +4,15 @@ from .models import Order
 
 
 class OrderFilter(django_filters.FilterSet):
+    
+    start_date = django_filters.DateFilter(
+        field_name="date_created", lookup_expr='gte')
+    end_date = django_filters.DateFilter(
+        field_name="date_created", lookup_expr='lte')
+
+    product__category = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Order
         fields = "__all__"
+        exclude = ['customer', 'date_created']
